13a99c2aa814f35208c57256d149dd8d1037d570
e <15262870437@163.com>
2018-02-14 16:27:53 +0800

    no message

--- blog/src/App.vue
+++ blog/src/App.vue
@@ -1,5 +1,35 @@
 <template>
   <div class="layout">
-    <router-view></router-view>
+    <mu-circular-progress :size="60" :strokeWidth="5" v-if="show" />
+    <router-view>
+    </router-view>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      show: false
+    }
+  },
+  created () {
+    this.axios.interceptors.request.use((config) => {
+      this.show = true
+      return config
+    })
+    this.axios.interceptors.response.use((config) => {
+      this.show = false
+      return config
+    })
+  }
+}
+</script>
+
+<style scoped>
+  .mu-circular-progress {
+    position: fixed;
+    left: 45%;
+    top: 45%;
+  }
+</style>
